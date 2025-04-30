5ca295c533b9b5ac1b207e18a83985330f1afffa
ankit saini <ankit@robu.in>
2025-05-01 01:04:06 +0530

    Signup Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import { LOGO } from "../Utils/constants";
 const Header = () => {
+    const [btnNameReact, setBtnNameReact] = useState('Sign In');
     return (
-      <div className="header">
-        <div className="app-logo">
-          <img
-            src={LOGO}
-          />
+        <div className="header">
+            <div className="app-logo">
+                <img
+                    src={LOGO}
+                />
+            </div>
+            <div className="header-menu">
+                <ul className="nav-items">
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                    <div className="signin-btn">
+                    <button className="signup-btn" onClick={() => {
+                        // let btnValue = "Sign Up";
+                        btnNameReact === "Sign In" ? setBtnNameReact('Sign Up') : setBtnNameReact('Sign In');
+                    }}>{btnNameReact}</button>
+                    </div>
+                </ul>
+            </div>
         </div>
-        <div className="header-menu">
-          <ul className="nav-items">
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
     );
-  };
+};
 
-  export default Header;
+export default Header;
